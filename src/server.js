86b354c82f33152c6113e79ad74ce05702aa8148
lamphat123 <phattlgbc200046@fpt.edu.vn>
2024-07-02 10:55:07 +0700

    create website

---
+++ src/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const ejs = require("ejs");
+const path = require("path");
+// methodOverride = require("method-override");
+
+const port = 8080;
+const db = require("./config/db/index");
+
+//Connect to DB
+// db.connect();
+
+const route = require("./router/index");
+
+app.use(express.json());
+
+// static
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+//morgan
+app.use(morgan("combined"));
+// app.use(methodOverride('_method'))
+//temples engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "app/view"));
+// const expressLayouts = require("express-ejs-layouts");
+// app.use(expressLayouts);
+
+// Routes init
+route(app);
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
